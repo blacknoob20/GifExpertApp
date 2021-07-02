7f12e25cbe2f5e50889492b49126ff0c4d2f4da3
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-07-01 23:10:12 -0500

    Pruebas con el helper getGifs Instalacion de el fetch API en getGifs.js

--- src/components/helpers/getGifs.js
+++ src/components/helpers/getGifs.js
@@ -1,20 +1,21 @@
+import fetch from 'node-fetch';
 
-    export const getGifs = async( categoria) => {
+export const getGifs = async( categoria) => {
 
-        // useEffect(()=>{
-        //     setImgs();
-        // }, []);
+    // useEffect(()=>{
+    //     setImgs();
+    // }, []);
 
-        const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(categoria)}&api_key=vFOCAMHBf2Il9PlbnNKjClm0zDSKSqqF`;
-        const res = await fetch(url);
-        const {data} = await res.json();
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images.downsized_medium.url
-            }
-        })
+    const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(categoria)}&api_key=vFOCAMHBf2Il9PlbnNKjClm0zDSKSqqF`;
+    const res = await fetch(url);
+    const {data} = await res.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
 
-        return(gifs);
-    }
+    return(gifs);
+}
---
+++ src/test/components/helpers/getGifs.test.js
@@ -0,0 +1,16 @@
+import { getGifs } from '../../../components/helpers/getGifs';
+
+
+describe('Pruebas con getGifs Fetch', () => {
+    test('Debe traer 10 elementos', async() => {
+        const gifs = await getGifs('One Punch');
+
+        expect(gifs.length).toBe(10);
+    });
+    
+    test('Debe traer 10 elementos', async() => {
+        const gifs = await getGifs('');
+
+        expect(gifs.length).toBe(0);
+    });
+});
